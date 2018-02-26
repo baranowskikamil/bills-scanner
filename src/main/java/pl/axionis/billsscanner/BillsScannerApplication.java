ebf527fb8c29c17afbaf6de9dc582250b5d0b68b
Kamil 'Qenlin' Baranowski <0baranowski.kamil@gmail.com>
2018-02-26 18:12:06 +0100

    Initialize of bills-scanner project

---
+++ src/main/java/pl/axionis/billsscanner/BillsScannerApplication.java
@@ -0,0 +1,12 @@
+package pl.axionis.billsscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsScannerApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/axionis/billsscanner/BillsScannerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.axionis.billsscanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BillsScannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
